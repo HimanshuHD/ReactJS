6fc182147d128e41cfb00b05ecc8673346fe6e47
Himanshu Digari <himanshudigari@gmail.com>
2023-08-05 11:02:33 +0530

    First React Page

---
+++ first-react-website/index.js
@@ -0,0 +1,9 @@
+console.log('Hello ReactJS...');
+ReactDOM.render(<p>Hello React 111!</p>, document.getElementById('app')); 
+ReactDOM.render(
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+    </ul>,
+    document.getElementById('list')
+);
